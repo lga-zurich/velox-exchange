9911f17cc5f8112bc1b74b726f043ad8437e1c0f
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2025-09-11 13:04:19 +0200

    upgrade ucx to 1.19.0 and UCXX to 0.45.01
    (cherry picked from commit c3e4121231b96d48693ce864ca5c5840c0d7b72c)

--- CMake/resolve_dependency_modules/cudf.cmake
+++ CMake/resolve_dependency_modules/cudf.cmake
@@ -62,6 +62,13 @@ set(
 )
 velox_resolve_dependency_url(cudf)
 
+set(VELOXX_ucxx_VERSION 0.45)
+set(VELOX_ucxx_BUILD_SHA256_CHECKSUM
+    e603bf66a1232c4fabb666d1b4860a8ad45eda53ec92d92e830ce126eac9f912)
+set(VELOX_ucxx_SOURCE_URL
+    "https://github.com/rapidsai/ucxx/archive/refs/tags/v0.45.01.tar.gz")
+velox_resolve_dependency_url(ucxx)
+
 # Use block so we don't leak variables
 block(SCOPE_FOR VARIABLES)
   # Setup libcudf build to not have testing components
@@ -104,6 +111,12 @@ block(SCOPE_FOR VARIABLES)
   )
 
   FetchContent_MakeAvailable(cudf)
+FetchContent_Declare(
+  ucxx
+  URL ${VELOX_ucxx_SOURCE_URL}
+  URL_HASH ${VELOX_ucxx_BUILD_SHA256_CHECKSUM}
+  SOURCE_SUBDIR cpp
+  UPDATE_DISCONNECTED 1)
 
   # cudf sets all warnings as errors, and therefore fails to compile with velox
   # expanded set of warnings. We selectively disable problematic warnings just for

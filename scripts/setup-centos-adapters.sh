b56edb762a3fe86ee45b4378f46de1336589b051
Luis Garcés-Erice <lga@zurich.ibm.com>
2025-09-19 16:01:52 +0200

    Allow building rapidsai presto in our dependency image
    (cherry picked from commit f3831454cf2834ec907a70ae8defb0e4d1cb8859)

--- scripts/setup-centos-adapters.sh
+++ scripts/setup-centos-adapters.sh
@@ -91,6 +91,7 @@ function install_cuda {
     libcufile-devel-"$dashed" \
     libnvjitlink-devel-"$dashed" \
     cuda-nvml-devel-"$dashed" \
+    libnvjitlink-devel-"$dashed" \
     numactl-devel
 }
 
